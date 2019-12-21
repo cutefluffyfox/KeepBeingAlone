ab2454b3eafbccd5b80b2a82bae17d49df6b421f
Mishanya from ML <mishkazel1@gmail.com>
2019-12-21 21:57:20 +0300

    Full screen changed to no frame in case of optimisation

--- script/main.py
+++ script/main.py
@@ -6,17 +6,10 @@ environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 user32 = ctypes.windll.user32
-
-SCREEN_INFO = {
-    "pygame_size": screen.get_size(),
-    "system_size": (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
-}
-
-width = (SCREEN_INFO["pygame_size"][0] + SCREEN_INFO["system_size"][0]) // 2 - 1
-height = (SCREEN_INFO["pygame_size"][1] + SCREEN_INFO["system_size"][1]) // 2 - 1
-screensize = (width, height)
+SYS_SIZE = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+screensize = width, height = SYS_SIZE
+screen = pygame.display.set_mode(screensize, pygame.NOFRAME)
 
 
 full_screen = True
@@ -36,15 +29,15 @@ while running:
                 height = screen.get_height()
                 full_screen = False
             elif event.key == pygame.K_F11 and not full_screen:
-                pygame.display.set_mode(SCREEN_INFO["pygame_size"], pygame.FULLSCREEN)
-                width = (SCREEN_INFO["pygame_size"][0] + SCREEN_INFO["system_size"][0]) // 2 - 1
-                height = (SCREEN_INFO["pygame_size"][1] + SCREEN_INFO["system_size"][1]) // 2 - 1
+                pygame.display.set_mode(SYS_SIZE, pygame.NOFRAME)
+                width, height = SYS_SIZE
                 full_screen = True
         if event.type == pygame.VIDEORESIZE and not full_screen:
             pygame.display.set_mode(event.size, pygame.RESIZABLE)
             width = screen.get_width()
             height = screen.get_height()
-    pygame.draw.rect(screen, (255, 255, 255), (width - 100, height - 100, 100, 100))
+    pygame.draw.rect(screen, (255, 255, 255), (width - 200, height - 200, 100, 100))
+    pygame.draw.rect(screen, (255, 255, 255), (100, 100, 100, 100))
     pygame.display.flip()
 
 pygame.quit()
